5f4d97341bf77f4380ca416a713f13ffb00af3db
busi-reddy-karnati <busisatya@gmail.com>
2022-01-05 01:19:09 -0500

    First Smart Contract

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.11;
 
 contract SimpleStorage{
 
-    // This is the way to declare a variable 
+    // This is the way to declare a variable in solidity
     uint x;
 
     // Format of the function is 
